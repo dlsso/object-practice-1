d8325abdfa5da9cf7d9f9cd628b09585326df365
dlsso <supertonesfan@hotmail.com>
2014-07-10 09:39:57 -0600

    Added earth, sky, icons, namespacing, init

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+// create a MoonGame namespace to contain all the game variables
+// use the revealing module pattern with IIFE
+// IIFE: function expression, surrounded by (), then immediately invoked: ()
+var MoonGame = (function() {
+
+	// declare array of free-flying birds
+	var birds = [];
+	var flock = null;
+
+	var init = function() {
+		console.log("test")
+	}
+
+
+	// return an object literal with the properties and methods that we want to reveal to the rest of the program
+	// everything else remains private
+	return {
+		init: init
+	}
+})();
+
+
+
 $(document).on('ready', function() {
-  
+	MoonGame.init();
 });
